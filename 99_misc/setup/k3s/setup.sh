fe956bc0e960f85f2115a1124f1d3a9af7d1078e
silent-mobius <alex@vaiolabs.com>
2023-11-16 23:19:38 +0200

    adding initial k3s for future labs

---
+++ 99_misc/setup/k3s/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+###########################################
+# Create by: Silent-Mobius aka Alex M. Schapelle
+# Purpose: setup awx on k3s
+# Version: 0.1.0
+# Date: 12.11.2023
+###########################################
+
+
