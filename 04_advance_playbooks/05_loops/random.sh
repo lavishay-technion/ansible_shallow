df26b972dd2c873ca75882676462d6f6a4c48315
silent-mobius <alex@vaiolabs.com>
2023-11-22 13:20:21 +0200

    adding loop examples

---
+++ 04_advance_playbooks/05_loops/random.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#########################################
+# Created by: Silent-Mobius
+# Purpose: ansible until loop example
+# Date": 10.11.2023
+# version: 0.1.0
+
+echo $((1 + RANDOM % 10))
