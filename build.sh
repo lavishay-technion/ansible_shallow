174b39d4e352244f518f4d9cc37e061e0757526e
Silent Mobius <alex@mobiusdevteam.com>
2023-12-07 16:19:19 +0000

    Update 3 files
    - /LICENSE
    - /build.sh
    - /00_intro/README.md

--- build.sh
+++ build.sh
@@ -7,6 +7,23 @@ set -o pipefail
 #created by: Silent-Mobius aka Alex M. Schapelle for Vaiolabs ltd.
 #purpose: build script for jetporch class
 #verion: 0.8.11
+# Created By: Silent-Mobius Aka Alex M. Schapelle
+# Purpose: being lazy on new course setup
+# Copyright (C) 2024 Alex M. Schapelle
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #########################################
 
 . /etc/os-release
