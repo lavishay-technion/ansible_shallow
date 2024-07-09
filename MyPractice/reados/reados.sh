734e8540ca1550f3ca558d1ad07f7d046e48c735
lavishay-technion <lavishay@campus.technion.ac.il>
2024-07-09 19:23:02 +0300

    Completed RunScripts.yaml

--- MyPractice/reados/reados.sh
+++ MyPractice/reados/reados.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 # This script checks the OS of the linux distribution and prints it out
-
 . /etc/os-release
-
 printf "This Linux distribution is %s And it's version is: %s\n"  $ID $VERSION_ID
